b64e9e764793dd5b7731a21ff4a35466eb6737af
Andrej Gasic <andrej@gasic.rs>
2024-10-17 07:07:00 +0200

    Read presets from files

--- starfield_core.py
+++ starfield_core.py
@@ -1,33 +1,31 @@
 import ai
 import starfield_structure
+import os
 import pydirectinput
 
 
 def generate_training_data(presets):
     embeddings = []
-    starfield_presets_path = r"C:\Users\andro\OneDrive\Documents\My Games\Starfield\SFSE\Plugins\Chargen\Presets\preset.npc"
-    frame = {"top": 260, "left": 1250, "width": 900, "height": 900}
-    vertical_center = frame["top"] + frame["height"] / 2
-    horizontal_center = frame["left"] + frame["width"] / 2
-    horizontal_start = frame["left"]
-    horizontal_end = frame["left"] + frame["width"]
     with ai.screenshot_context() as sct:
-        for preset in presets:
-            preset.to_file(starfield_presets_path)
-            print("Beginning input")
-            pydirectinput.press("b")  # delete
-            pydirectinput.press("enter")
-            pydirectinput.press("enter")  # F9, save is F5
-            pydirectinput.press("t")  # Body
-            pydirectinput.press("t")  # Face
+        for i, preset in enumerate(presets):
+            preset.to_file(os.path.join(starfield_presets_path, f"preset{i}.npc"))
+            pydirectinput.press("b", duration=0.1)  # delete
+            pydirectinput.press("enter", duration=0.1)
+            pydirectinput.press("enter", duration=0.1)  # F9, save is F5
+            pydirectinput.press("t", duration=0.1)  # Body
+            pydirectinput.press("t", duration=0.1)  # Face
 
-            # ai.show_capture(sct, frame)
+            ai.show_capture(sct, frame)
             forward_embedding = ai.calculate_embedding(sct, frame)
-            pydirectinput.moveTo(horizontal_end, vertical_center)
-            pydirectinput.dragTo(horizontal_center, vertical_center, button="right")
+            pydirectinput.moveTo(horizontal_end, vertical_center, duration=1)
+            pydirectinput.dragTo(
+                horizontal_center, vertical_center, button="right", duration=1
+            )
             right_embedding = ai.calculate_embedding(sct, frame)
-            pydirectinput.moveTo(horizontal_start, vertical_center)
-            pydirectinput.dragTo(horizontal_end, vertical_center, button="right")
+            pydirectinput.moveTo(horizontal_start, vertical_center, duration=1)
+            pydirectinput.dragTo(
+                horizontal_end, vertical_center, button="right", duration=1
+            )
             left_embedding = ai.calculate_embedding(sct, frame)
 
             embedding = ai.calculate_combined_embedding(
@@ -35,15 +33,23 @@ def generate_training_data(presets):
             )
             embeddings.append(embedding)
 
-            pydirectinput.press("q")  # Body
-            pydirectinput.press("q")  # Presets
+            pydirectinput.press("q", duration=0.1)  # Body
+            pydirectinput.press("q", duration=0.1)  # Presets
     return list(zip(embeddings, presets))
 
 
 pydirectinput.PAUSE = 1
 
-presets = [starfield_structure.StarfieldCharacter()]
+starfield_presets_path = (
+    r"C:\Users\andro\OneDrive\Documents\My Games\Starfield\SFSE\Plugins\Chargen\Presets"
+)
 
-training_data = generate_training_data(presets)
+frame = {"top": 260, "left": 1250, "width": 900, "height": 900}
+vertical_center = frame["top"] + frame["height"] / 2
+horizontal_center = frame["left"] + frame["width"] / 2
+horizontal_start = frame["left"]
+horizontal_end = frame["left"] + frame["width"]
+
+training_data = generate_training_data([starfield_structure.StarfieldCharacter()])
 
 print(training_data)
--- starfield_structure.py
+++ starfield_structure.py
@@ -14,90 +14,154 @@ def one_hot_decode(it, length):
     return one_hot_vector.index(1)
 
 
-def generate_random():
-    categorical_indices = (
-        one_hot_encode(
-            random.randint(0, len(starfield_constants.hair_brow_colours) - 1),
-            len(starfield_constants.hair_brow_colours),
-        )
-        + one_hot_encode(
-            random.randint(0, len(starfield_constants.eye_colours) - 1),
-            len(starfield_constants.eye_colours),
-        )
-        + one_hot_encode(
-            random.randint(0, len(starfield_constants.hair_brow_colours) - 1),
-            len(starfield_constants.hair_brow_colours),
-        )
-        + one_hot_encode(
-            random.randint(0, len(starfield_constants.skin_tones) - 1),
-            len(starfield_constants.skin_tones),
-        )
-        + one_hot_encode(
-            random.randint(0, len(starfield_constants.hair_styles) - 1),
-            len(starfield_constants.hair_styles),
-        )
-        + one_hot_encode(
-            random.randint(0, len(starfield_constants.brow_styles) - 1),
-            len(starfield_constants.brow_styles),
-        )
-    )
-    feature_bones_values = [
-        random.random()
-        for _ in starfield_constants.feature_regions
-        for _ in starfield_constants.features
-    ]
-    additional_bones_values = [
-        random.random()
-        for ids in starfield_constants.additional_regions.values()
-        for _ in ids
-    ]
-    sliders_values = [
-        random.random()
-        for _ in starfield_constants.variations
-        for _ in starfield_constants.features
-    ]
-    return (
-        categorical_indices
-        + feature_bones_values
-        + additional_bones_values
-        + sliders_values
-    )
-
-
 class StarfieldCharacter:
-    def __init__(self, result=generate_random()):
+    def __init__(
+        self,
+        brow_hair_colour,
+        eye_colour,
+        hair_colour,
+        skin_tone,
+        hair,
+        eyebrow,
+        feature_bones,
+        additional_bones,
+        sliders,
+    ):
+        self.brow_hair_colour = brow_hair_colour
+        self.eye_colour = eye_colour
+        self.hair_colour = hair_colour
+        self.skin_tone = skin_tone
+        self.hair = hair
+        self.eyebrow = eyebrow
+        self.feature_bones = feature_bones
+        self.additional_bones = additional_bones
+        self.sliders = sliders
+
+    @classmethod
+    def from_random(cls):
+        brow_hair_colour = random.choice(starfield_constants.hair_brow_colours)
+        eye_colour = random.choice(starfield_constants.eye_colours)
+        hair_colour = random.choice(starfield_constants.hair_brow_colours)
+        skin_tone = random.choice(starfield_constants.skin_tones)
+        hair = random.choice(starfield_constants.hair_styles)
+        eyebrow = random.choice(starfield_constants.brow_styles)
+        feature_bones = {
+            region: {
+                feature: random.random() for feature in starfield_constants.features
+            }
+            for region in starfield_constants.feature_regions
+        }
+        additional_bones = {
+            region: {id: random.random() for id in ids}
+            for region, ids in starfield_constants.additional_regions.items()
+        }
+        sliders = {
+            variation + feature: random.random()
+            for variation in starfield_constants.variations
+            for feature in starfield_constants.features
+        }
+        return cls(
+            brow_hair_colour,
+            eye_colour,
+            hair_colour,
+            skin_tone,
+            hair,
+            eyebrow,
+            feature_bones,
+            additional_bones,
+            sliders,
+        )
+
+    @classmethod
+    def from_result(cls, result):
         it = iter(result)
-        self.brow_hair_colour = starfield_constants.hair_brow_colours[
+        brow_hair_colour = starfield_constants.hair_brow_colours[
             one_hot_decode(it, len(starfield_constants.hair_brow_colours))
         ]
-        self.eye_colour = starfield_constants.eye_colours[
+        eye_colour = starfield_constants.eye_colours[
             one_hot_decode(it, len(starfield_constants.eye_colours))
         ]
-        self.hair_colour = starfield_constants.hair_brow_colours[
+        hair_colour = starfield_constants.hair_brow_colours[
             one_hot_decode(it, len(starfield_constants.hair_brow_colours))
         ]
-        self.skin_tone = starfield_constants.skin_tones[
+        skin_tone = starfield_constants.skin_tones[
             one_hot_decode(it, len(starfield_constants.skin_tones))
         ]
-        self.hair = starfield_constants.hair_styles[
+        hair = starfield_constants.hair_styles[
             one_hot_decode(it, len(starfield_constants.hair_styles))
         ]
-        self.eyebrow = starfield_constants.brow_styles[
+        eyebrow = starfield_constants.brow_styles[
             one_hot_decode(it, len(starfield_constants.brow_styles))
         ]
-        self.feature_bones = {
+        feature_bones = {
             region: {feature: next(it) for feature in starfield_constants.features}
             for region in starfield_constants.feature_regions
         }
-        self.additional_bones = {
+        additional_bones = {
             region: {id: next(it) for id in ids}
             for region, ids in starfield_constants.additional_regions.items()
         }
-        self.sliders = {
+        sliders = {
             variation + "_" + feature: next(it)
             for variation in starfield_constants.variations
             for feature in starfield_constants.features
         }
+        return cls(
+            brow_hair_colour,
+            eye_colour,
+            hair_colour,
+            skin_tone,
+            hair,
+            eyebrow,
+            feature_bones,
+            additional_bones,
+            sliders,
+        )
+
+    @classmethod
+    def from_format(cls, format):
+        brow_hair_colour = format["BrowHairColor"]
+        eye_colour = format["EyeColor"]
+        hair_colour = format["HairColor"]
+        skin_tone = format["SkinTone"]
+        hair = format["UniqueHeadPartsA"][3].removeprefix("Human_Female_Hair_")
+        eyebrow = format["UniqueHeadPartsA"][6].removeprefix("Human_Female_Eyebrow_")
+        feature_bones = {
+            item["RegionID"]: {
+                nested_item["GroupName"]: nested_item["Value"]
+                for nested_item in item["SlidersA"]
+            }
+            for item in format["FacialBoneRegionDataA"]
+            if item["RegionID"] in starfield_constants.feature_regions
+        }
+        additional_bones = {
+            item["RegionID"]: {
+                nested_item["ID"]: nested_item["Value"]
+                for nested_item in item["SlidersA"]
+            }
+            for item in format["FacialBoneRegionDataA"]
+            if item["RegionID"] not in starfield_constants.feature_regions
+        }
+        sliders = {
+            item["Name"].removeprefix("female_"): item["Value"]
+            for item in format["FacialMorphSliderDataA"]
+        }
+        return cls(
+            brow_hair_colour,
+            eye_colour,
+            hair_colour,
+            skin_tone,
+            hair,
+            eyebrow,
+            feature_bones,
+            additional_bones,
+            sliders,
+        )
+
+    @classmethod
+    def from_file(cls, path):
+        with open(path, "r") as json_file:
+            return cls.from_format(json.load(json_file))
 
     def to_result(self):
         return (
