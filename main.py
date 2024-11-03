4ecfe82c9fa18e3024bf822f7b08a137f7f39aaa
Andrej Gasic <andrej@gasic.rs>
2024-11-03 02:25:51 +0100

    Save generated presets together

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+import numpy
 import ai
 from starfield import automation, data
 
 
-def generate_training_data(presets):
-    embeddings = []
+def create_input_result(presets):
+    embeddings = {}
     with ai.screenshot_context() as sct:
-        for i, preset in enumerate(presets):
+        for i, preset in enumerate(presets.values()):
             automation.load_preset(preset, f"preset{i}.npc")
             automation.to_chargen_from_presets()
 
             embedding = ai.calculate_embedding(sct, automation.frame)
-            embeddings.append(embedding)
+            embeddings[f"{i}-embedding"] = embedding
 
             automation.to_presets_from_chargen()
-    return list(zip(embeddings, presets))
+    return {**embeddings, **presets}
 
 
-training_data = generate_training_data([data.Character.from_random()])
+def generate_data(num, path):
+    presets = {
+        f"{i}-preset": numpy.array(data.Character.from_random()) for i in range(num)
+    }
+    pairs = create_input_result(presets)
+    numpy.savez_compressed(path, **pairs)
 
-# print(training_data)
+
+def load_data(path):
+    pairs = numpy.load(path)
+    return [
+        (pairs[f"{i}-preset"], pairs[f"{i}-embedding"]) for i in range(len(pairs) // 2)
+    ]
+
+
+generate_data(20, "training.npz")
+
+generate_data(5, "validation.npz")
